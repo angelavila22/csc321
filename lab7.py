646d61735f55872fb68b3f42137e9db30d3e6e36
angelavila22 <angelavilla92205@gmail.com>
2026-02-25 02:12:45 +0000

    Added Python file

---
+++ lab7.py
@@ -0,0 +1,18 @@
+x = 5
+y = 2.26
+z = "Charles"
+
+if x == 26:
+    print("X is 2.6")
+else:
+    print("X is not 2.6")
+
+if y == 2.26:
+    print("Y is 2.26")
+else:
+     print("Y is not 2.26")
+
+if z == "Charles":
+    print("Z is Angel")
+else:
+    print("Z is not Charles")
